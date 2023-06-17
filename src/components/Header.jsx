f5dcb63e511f671d0ee37738b36a5408021c3da4
delso-ferreira <delsofelipe@gmail.com>
2023-06-17 15:12:10 -0300

    att

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import planetContext from '../context/planetContext';
 
 function Header() {
   const { setSearch, setActiveFilters, activeFilters } = useContext(planetContext);
-  const [selectOptions, setSelectOptions] = useState({
-    column: 'population',
-    comparacao: 'maior que',
-    valor: 0,
-  });
 
   const columnOptions = ['population', 'orbital_period',
     'rotation_period', 'diameter', 'surface_water'];
 
-  /* const [column, setColumn] = useState(columnOptions); */
+  const [column, setColumn] = useState(columnOptions);
+
+  const [selectOptions, setSelectOptions] = useState({
+    column: column[0],
+    comparacao: 'maior que',
+    valor: 0,
+  });
 
   const handleNameFilter = (event) => {
     setSearch(event.target.value);
@@ -23,12 +24,27 @@ function Header() {
     setSelectOptions((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  const handleSubmit = () => {
-    setActiveFilters((previous) => ([...previous, selectOptions]));
-  };
-
   const handleColumnChoices = (iColumn) => !activeFilters
     .some((filter) => iColumn === filter.column);
+
+  const test = () => {
+    for (let i = 0; i < columnOptions.length; i += 1) {
+      columnOptions.find((c) => (c === c[i]));
+    }
+  };
+
+  const handleSubmit = () => {
+    setActiveFilters((previous) => ([...previous, selectOptions]));
+    setColumn(handleColumnChoices);
+  };
+
+  useEffect(() => {
+    setSelectOptions({
+      column: test,
+      comparacao: 'maior que',
+      valor: 0,
+    });
+  }, [column]);
 
   return (
     <div>
